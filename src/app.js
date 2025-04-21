7a38c377a85118f57c4784cf4b47a17368dfe036
Divya Wadhwa <wadhwadivya01@gmail.com>
2025-04-22 01:22:18 +0530

    created an express server

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/',(req, res)=>{
+    res.send("hello jiiiiii hiiiiiiii  dashboard");
+})
+
+app.use('/test',(req, res)=>{
+    res.send("response from server");
+})
+
+app.use('/hello',(req, res)=>{
+    res.send("hello hello hello");
+})
+
+app.listen(3001,()=>{
+    console.log("Server is successfully listening on port 3001..");
+});
+
+
+
+
